840d6a1865eec04fcd7c9256fc096c0f7cc82830
lorenaaseabra <lorenamseabra@gmail.com>
2023-05-22 13:29:32 +0100

    att

---
+++ login.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+
+ini_set('default_charset', 'UTF-8');
+
+include ("databaseConnect.php");
+
+$nomeErr = $emailErr = $passwordErr= "";
+$nome = $email = $password = $hidden = $disabled = "";
+
+
+function test_input($dados) {
+	$dados = trim($dados);
+	$dados = stripslashes($dados);
+	$dados = htmlspecialchars($dados);
+	return $dados;
+  }
+
+if( !empty( $_SESSION['login'] )){
+    header ('Location: index.php');
+} else {
+
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    if (empty($_POST["email"])) {
+      $emailErr = "Email is required!";
+    } else {
+      $email = test_input($_POST["email"]);
+      // check if e-mail address is well-formed
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+      }
+    }
+
+    if (empty($_POST["password"])) {
+      $nomeErr = "Password is required!";
+    } else {
+      $nome = test_input($_POST["password"]);
+    }
+    
+    if ($passwordErr =="" AND $emailErr == ""){
+      $query = "SELECT * FROM contatos WHERE email='$_POST[email]' AND  password='$_POST[password]'";
+      $result = mysqli_query ($conn,$query);
+      $row = mysqli_fetch_assoc ($result);
+      if (mysqli_num_rows($result) > 0){
+        $_SESSION['nome'] = $row['nome'];
+        $_SESSION['login'] = TRUE;
+        header ('Location: index.php');
+      } else {
+        $autErr ="Please verify you authentication data!";
+      }
+  
+    }
+  }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=ISO8859-1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- general css -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- own css -->
+    <link rel="stylesheet" href="./css/registration.css">
+
+    <title>Login Page</title> 
+  </head>
+
+  <body>
+    <main>
+
+      <!-- info -->
+      <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST" AND ($passwordErr !="" OR $emailErr != "" OR $autErr !="")) {
+      ?>
+      <div>
+        <h4>Alert!</h4>
+        <hr>
+        <?php
+          echo $autErr;
+          echo $emailErr;
+          echo $passwordErr;
+        ?>
+      </div>
+      <?php } ?><!-- /.info -->
+
+      <div class="container">
+        <div>
+            <h2>Login</h2>
+        </div>
+        <div>
+            <form>
+                <div class="input-item">
+                    <label for="email">Email: </label><br>
+                    <input type="email" id="email" name="email">
+                </div>
+                <br>
+                <div class="input-item">
+                    <label for="password">Password: </label><br>
+                    <input type="password" id="password" name="password">
+                </div>
+                <br>
+                <div>
+                    <input id="btn-submit" type="submit" value="Submit">
+                </div>
+            </form>
+        </div>
+    </div>
+    <a href="profile.html">Clicar aqui para testar a prox página</a>
+    </main>
+
+  </body>
+</html>
+<?php
+  mysqli_close($conn);
+?>
---
+++ profile.php
@@ -0,0 +1,152 @@
+<?php
+//initialize session
+session_start();
+
+// PHP charset
+ini_set('default_charset', 'UTF-8');
+
+if( $_SESSION['login'] == TRUE){
+
+// database connection
+include ("databaseConnect.php");
+
+// intialize variables
+$nomeErr = $emailErr = "";
+$nome = $email = "";
+
+switch ($_SERVER["REQUEST_METHOD"]){
+	case 'POST':
+		$codigo = $_POST['codigo'];
+		break;
+	case 'GET':
+		$codigo = $_GET['codigo'];
+		break;
+}
+
+// "cleaning data"
+function test_input($dados) {
+	$dados = trim($dados);
+	$dados = stripslashes($dados);
+	$dados = htmlspecialchars($dados);
+	return $dados;
+  }
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+	if (empty($_POST["nome"])) {
+		$nomeErr = "<strong>Do not remove the Name.</strong> Please insert a valid Name!";
+	} else {
+		$nome = test_input($_POST["nome"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^([^[:punct:]\d]+)$/",$nome)) {
+		  $nomeErr = "Only letters and white space allowed!";
+		}
+	}
+	  
+	if (empty($_POST["email"])) {
+		$emailErr = "<strong>Do not remove Email.</strong> Please insert a valid Email!";
+	} else {
+    $email = test_input($_POST["email"]);
+    // verifica o formato do email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format!";
+		}
+	}
+
+	if ($nomeErr =="" AND $emailErr == ""){
+		$query = "UPDATE contatos SET nome = '$nome', email = '$email' WHERE codigo = $codigo";
+		$result = mysqli_query ($conn, $query);	
+	}
+
+}
+
+$query = "SELECT * FROM contatos WHERE codigo=$codigo";
+$result = mysqli_query ($conn, $query);
+$row = mysqli_fetch_assoc ($result);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- insert here the reference to stylesheet file -->
+    
+    <!-- general css -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- own css -->
+    <link rel="stylesheet" href="./css/profile.css">
+    <title>User profile</title>
+  </head>
+
+  <body>
+
+    <main>
+      <div> 
+        <legend><strong>Update</strong></legend>
+      </div>
+
+    <div><!-- info -->
+		<?php
+		  	if($_SERVER["REQUEST_METHOD"] == "POST" AND $nomeErr =="" AND $emailErr == "") {
+		?>
+          <div >
+            <h4>Info!</h4>
+            <hr>
+            Data were updated.
+          </div>
+        <?php
+            }	
+        ?>
+		<?php if($nomeErr !="" OR $emailErr != "") { ?>
+            <div">
+			<h4>Alert!</h4>
+              <hr>
+              <p><?PHP echo $nomeErr ?></p>
+              <p><?PHP echo $emailErr ?></p> 
+            </div>
+        <?php }	?>
+    </div><!-- /.info -->
+
+    <div><!-- contentor do formulario --> 
+        <form name="frmInserir" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <div>
+              <label for="nome">Name </label>
+              <div>
+                <input name="nome" type="text" value="<?php echo $row['nome'];?>" placeholder="Name"/>
+              </div>
+            </div>
+            <div>
+              <label for="email">Email </label>
+              <div>
+                <input name="email" type="email" value="<?php echo $row['email'];?>" placeholder="Email"/>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <input name="codigo" type="hidden" value="<?PHP echo $codigo; ?>" />
+                  <button name="alterar" type="submit" >Save</button>
+                  <button name="limpar" type="reset" >Reset</button>
+                  <a href="list.php">Back to List</a>
+                </div>
+              </div>
+            </div>
+        </form>
+      </div>
+    </main>	
+	</body>
+</html>
+<?php
+// close connection
+mysqli_close ($conn);
+
+} else {
+  header ('Location: login.php');
+} 
+?>
