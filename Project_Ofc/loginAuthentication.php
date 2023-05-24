156f2a2f9799c1fb6536dd7700dc649600264873
unknown <nanavcouto17@gmail.com>
2023-05-24 13:30:17 +0100

    att

--- Project_Ofc/loginAuthentication.php
+++ Project_Ofc/loginAuthentication.php
@@ -69,20 +69,8 @@ if(!empty( $_SESSION['login'])){
     <meta name="description" content="">
     <meta name="author" content="">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
-    <style>
-      body {
-        margin-left: 10px;
-        background: rgb(9,9,121);
-        background: linear-gradient(90deg, rgba(9,9,121,1) 35%, rgba(0,174,209,1) 100%);
-        color: white;
-      }
-
-      .form-control {
-        width: 30%;
-      }
-      
-    </style>
+    <link rel="stylesheet" href="style.css">
+  
     <title>Login</title> 
   </head>
 
--- Project_Ofc/register.php
+++ Project_Ofc/register.php
@@ -72,48 +72,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <!-- insert here the reference to stylesheet file -->
-    <link href="" rel="stylesheet">
-    <title>EXEMPLE TO MANAGE DATABASE WITH PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Register</title>
   </head>
 
   <body>
-  <header>
-      <!-- navigation bar -->
-      <nav>
-        <a href="#">CRUD</a>
-        <div>
-          <ul>
-            <li>
-              <a href="index.php">Home</a>
-            </li>
-            <li>
-              <a href="read.php">List data</a>
-            </li>
-            <li >
-              <a href="register.php">Create new</a>
-            </li>
-            <li>
-              <a href="closeSession.php">End session</a>
-            </li>
-          </ul>
-
-          <!-- search form -->
-          <form name="frmPesquisa" method="post" action="read.php">
-            <input type="text" placeholder="Search" aria-label="Search" name="pesquisa">
-            <button type="submit">Search</button>
-          </form>
-
-        </div>
-      </nav>
-      <!-- /.navigation bar -->
-    </header>
     <main>
       <div> <!-- title -->
-        <legend><strong>Create</strong>RUD</legend>
+        <h1>Register</h1>
       </div>
 
-      <div><!-- info -->
+      <div>
         <?php
           if($_SERVER["REQUEST_METHOD"] == "POST" AND $nomeErr =="" AND $emailErr == "" AND $passwordErr =="") {
         ?>
@@ -128,53 +99,52 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         <?php if($nomeErr !="" OR $emailErr != "" OR $passwordErr !="") { ?>
           <div>
               <h4>Alert!</h4>
-              <hr>
               <p><?PHP echo $nomeErr ?></p>
               <p><?PHP echo $emailErr ?></p>
               <p><?PHP echo $passwordErr ?></p>
           </div>
         <?php }	?>
-      </div><!-- /.info -->
+      </div>
 
-      <div><!-- contentor do formulario --> 
+      <div> 
         <form name="frmInserir" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            <div>
-              <label>Name </label>
-              <div >
-                <input name="nome" type="text" value="<?php echo $nome;?>" placeholder="Name" <?php echo $disabled ?>>
-              </div>
+            <div class="mb-3">
+              <label class="form-label">Name: </label>
+              <input name="nome" type="text" class="form-control" value="<?php echo $nome;?>" placeholder="Name" <?php echo $disabled ?>>
             </div>
-            <div>
-              <label>Email </label>
-              <div>
-                <input name="email" type="email" value="<?php echo $email;?>" placeholder="Email" <?php echo $disabled ?>>
-              </div>
+
+            <div class="mb-3">
+              <label class="form-label">Email: </label>
+              <input name="email" type="email" class="form-control" value="<?php echo $email;?>" placeholder="Email" <?php echo $disabled ?>>
             </div>
-            <div <?php echo $hidden ?>>
-              <label>Password </label>
-              <div>
-                <input name="password" type="password" placeholder="Password [min. 5 characters]"/>
-              </div>
+
+            <div class="mb-3" <?php echo $hidden ?>>
+              <label class="form-label">Password: </label>
+              <input name="password" type="password" class="form-control" placeholder="Password [min. 5 characters]"/>
             </div>
-            <div <?php echo $hidden ?>>
-              <label>Repeat password </label>
-              <div>
-                <input name="rpassword" type="password" placeholder="Repeat password"/>
-              </div>
+
+            <div class="mb-3" <?php echo $hidden ?>>
+              <label class="form-label">Repeat password: </label>
+              <input name="rpassword" type="password" class="form-control" placeholder="Repeat password"/>
             </div>
-            <div>
-              <div>
-                <div>	
-                  <button name="gravar" type="submit" <?php echo $disabled ?>>Save</button>
-                  <button name="limpar" type="reset" >Reset</button>
-                  <a href="read.php">Back to list</a>
-                </div>
-              </div>
+
+            <div class="mb-3">
+              <label class="form-label">Birthdate: </label>
+              <input name="birthdate" type="date" class="form-control"/>
             </div>
+
+
+            <div>	
+              <button class="btn btn-primary" name="gravar" type="submit" <?php echo $disabled ?>>Save</button>
+              <button class="btn btn-primary" name="limpar" type="reset" >Reset</button>
+              <p>Already have an account? <a href="loginAuthentication.php">Login</a></p>
+            </div>
+
         </form>
-      </div><!-- /.container -->
+      </div>
 
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   </body>
 </html>
 <?php
